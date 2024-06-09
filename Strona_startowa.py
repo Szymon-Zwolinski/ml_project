71fb2bc80f6d915741768e10455b5e7f1c4e3f89
Szymon-Zwolinski <zwol.szymon@gmail.com>
2024-06-09 11:52:36 +0200

    Added files structure, pages and simple start page.

---
+++ Strona_startowa.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(page_title="Main page", page_icon="🏠")
+
+st.title("Metodami Scoringowe i Metody Klasyfikacji Obrazów")
+
+st.divider()
+
+st.write("""
+    Witamy na naszej stronie, która jest poświęcona metodom scoringowym i metodom klasyfikacji obrazów.
+    Możesz tutaj znaleźć narzędzia i techniki, które pomogą Ci w ocenie i klasyfikacji różnorodnych obrazów.
+    
+    W menu po lewej stronie możesz wybrać różne strony, aby dowiedzieć się więcej o poszczególnych metodach oraz zobaczyć ich praktyczne zastosowania.
+    """)
